3b3f109005940774c5e6173436268eee5c47a0fc
Ubuntu <ubuntu@ip-172-31-23-248.us-west-2.compute.internal>
2025-08-05 18:54:52 +0000

    Save X30 for one more subroutine

--- ed25519_s2n_bignum.h
+++ ed25519_s2n_bignum.h
@@ -18,7 +18,7 @@ int ed25519_verify_no_self_test_s2n_bignum(
 						const uint8_t signature[ED25519_SIGNATURE_LEN],
 						const uint8_t public_key[ED25519_PUBLIC_KEY_LEN]);
 
- int ed25519ctx_sign_no_self_test_s2n_bignum(
+int ed25519ctx_sign_no_self_test_s2n_bignum(
 						uint8_t out_sig[ED25519_SIGNATURE_LEN], const uint8_t *message,
 						size_t message_len, const uint8_t private_key[ED25519_PRIVATE_KEY_LEN],
 						const uint8_t *context, size_t context_len);
